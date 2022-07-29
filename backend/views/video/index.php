be54aad8ffc7984b3c6579cf48b8d87deb73a950
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-07-29 14:44:26 +0700

    add: video view

---
+++ backend/views/video/index.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Videos';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="video-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Video', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'video_id',
+            'title',
+            'tags',
+            'status',
+            'has_thumbnail',
+            //'video_name',
+            //'description:ntext',
+            //'created_by',
+            //'created_at',
+            //'updated_at',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Video $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'video_id' => $model->video_id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
