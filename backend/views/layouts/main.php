9ec654d34baae88bd531e31b8b06e8fcd3541e60
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-07-29 13:49:10 +0700

    update: change layout

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -7,8 +7,6 @@ use backend\assets\AppAsset;
 use common\widgets\Alert;
 use yii\bootstrap4\Breadcrumbs;
 use yii\bootstrap4\Html;
-use yii\bootstrap4\Nav;
-use yii\bootstrap4\NavBar;
 
 AppAsset::register($this);
 ?>
@@ -27,54 +25,23 @@ AppAsset::register($this);
 <body class="d-flex flex-column h-100">
     <?php $this->beginBody() ?>
 
-    <header>
-        <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = [
-            'label' => 'logout('.Yii::$app->user->identity->username.')',
-            'url' => ['/site/logout'],
-            'linkOptions' => [
-                'data-method' => 'post'
-            ],
-        ];
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
-    </header>
+    <div class="wrap h-100 d-flex flex-flex-column">
+        <!-- render file header -->
+        <?php echo $this->render('_header.php') ?>
 
-    <main role="main" class="flex-shrink-0">
-        <div class="container">
-            <?= Breadcrumbs::widget([
+        <!-- render file sidebar -->
+        <?php echo $this->render('_sidebar.php') ?>
+
+        <main role="main" class="d-flex">
+            <div class="container">
+                <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
-            <?= Alert::widget() ?>
-            <?= $content ?>
-        </div>
-    </main>
-
-    <footer class="footer mt-auto py-3 text-muted">
-        <div class="container">
-            <p class="float-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-            <p class="float-right"><?= Yii::powered() ?></p>
-        </div>
-    </footer>
-
+                <?= Alert::widget() ?>
+                <?= $content ?>
+            </div>
+        </main>
+    </div>
     <?php $this->endBody() ?>
 </body>
 
