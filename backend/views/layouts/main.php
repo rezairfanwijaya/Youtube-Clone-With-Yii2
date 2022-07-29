fd558f2cfa44e9b512d0992fd83da67cd000d458
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-07-29 17:10:18 +0700

    add: style in base main code

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -19,8 +19,38 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" rel="stylesheet">
     <?php $this->head() ?>
 </head>
+
+<style>
+    .upload-icon{
+        width: 200px;
+        height:200px;
+        border-radius: 50%;
+        background: #e0e0e0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 70px;
+        color: #454545;
+    }
+
+    .btn-file{
+        position: relative;
+    }
+
+    .btn-file input{
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+        opacity: 0%;
+    }
+</style>
 
 <body class="d-flex flex-column h-100">
     <?php $this->beginBody() ?>
@@ -32,7 +62,7 @@ AppAsset::register($this);
         <!-- render file sidebar -->
         <?php echo $this->render('_sidebar.php') ?>
 
-        <main role="main" class="d-flex">
+        <main role="main" class="d-flex w-100">
             <div class="container">
                 <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
